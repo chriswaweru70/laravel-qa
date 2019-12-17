ee1cfa606460f26b2e9318f3d34b37a5d0a2de55
Chris Waweru <chriswaweru70@gmail.com>
2019-12-17 13:00:04 +0300

    Tiding the views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,42 +10,10 @@
                     <hr>
                     @include ('layouts._messages')
                     @foreach ($answers as $answer)
-                        <div class="media">
-                            <div class="d-flex flex-column vote-controls">
-                                <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : ''}}"
-                            onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();">
-                                <i class="fas fa-thumbs-up fa-2x"></i>
-                            </a>
-                            <form id="up-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="POST" style="display none" >
-                                @csrf 
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="votes-count">{{ $answer->votes_count }}</span>
-                            <a title="This answer is not useful" class="vote-down {{ Auth::guest()? 'off': ''}}"
-                            onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();">
-                                    <i class="fas fa-thumbs-down fa-2x"></i>
-                            </a>
-                            <form id="down-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="POST" style="display none" >
-                                @csrf 
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                        @can('accept', $answer)
-                            <a title="Mark this answer as best answer" class=" {{ $answer->status }} mt-2" onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
-                                <i class="fas fa-check fa-2x"></i>
-                                <span class="favorites-count">123</span>
-                            </a>
-                                <form id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="POST" style="display none" >
-                                    @csrf 
-                                </form>
-                                @else
-                                @if ($answer->is_best)
-                            <a title="The question owner accepted this answer as best answer" class=" {{ $answer->status }} mt-2"> 
-                                <i class="fas fa-check fa-2x"></i>
-                                <span class="favorites-count">123</span>
-                            </a>
-                                @endif
-                        @endcan
-                        </div>
+                        <div class="media post">
+                           @include ('shared._vote', [
+                               'model' => $answer
+                           ])
                             <div class="media-body">
                                  {!! $answer->body_html !!}
                                  <div class="row">
@@ -72,8 +40,9 @@
                                         </div>
                                     </div>
                                 </div>
-                         </div>
-                         <hr>
+                            </div>
+                        
+                        @include ('answers._answer')
                     @endforeach
                 </div>
             </div>
