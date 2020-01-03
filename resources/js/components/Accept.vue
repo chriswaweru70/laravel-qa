90baf29d0dddca8e148fdbcb42dfa28ebca63d93
Chris Waweru <chriswaweru70@gmail.com>
2020-01-03 10:38:30 +0300

    Fixing issue on accept component using a global event bus

--- resources/js/components/Accept.vue
+++ resources/js/components/Accept.vue
@@ -21,6 +21,8 @@
 </template>
 
 <script>
+import EventBus from '../event-bus'
+
 export default {
 	props: ['answer'],
 	data() {
@@ -28,6 +30,11 @@ export default {
 			isBest: this.answer.is_best,
 			id: this.answer.id
 		}
+	},
+	created() {
+		EventBus.$on('accepted', id => {
+			this.isBest = id === this.id
+		})
 	},
 	methods: {
 		create() {
@@ -37,6 +44,8 @@ export default {
 					position: 'bottomLeft'
 				})
 				this.isBest = true
+
+				EventBus.$emit('accepted', this.id)
 			})
 		}
 	},
