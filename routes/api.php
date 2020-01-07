0b4bfee2aa0429d3965d4c3947127670f5611c5d
Chris Waweru <chriswaweru70@gmail.com>
2020-01-07 14:49:49 +0300

    Create endpoint to create question

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,11 @@ use Illuminate\Http\Request;
 Route::post('/token', 'Auth\LoginController@getToken');
 Route::get('/questions', 'Api\QuestionsController@index');
 
+Route::middleware(['auth:api'])->group(function () {
+    Route::apiResource('/questions', 'Api\QuestionsController')->except('index');
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
