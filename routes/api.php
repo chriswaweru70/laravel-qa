e5bf56694b1e017d91ecaad79a9f9766f1267129
Chris Waweru <chriswaweru70@gmail.com>
2020-01-08 12:58:37 +0300

    Testing api endpoints for both favoriting,unfavoriting a question and accepting an answer

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,16 @@ Route::get('/questions/{question}/answers', 'Api\AnswersController@index');
 Route::get('/questions/{question}-{slug}', 'Api\QuestionDetailsController');
 
 
+
 Route::middleware(['auth:api'])->group(function () {
     Route::apiResource('/questions', 'Api\QuestionsController')->except('index');
     Route::apiResource('/questions.answers', 'Api\AnswersController')->except('index');
     Route::post('/questions/{question}/vote', 'Api\VoteQuestionController');
     Route::post('/answers/{answer}/vote', 'Api\VoteAnswerController');
+    Route::post('/answers/{answer}/accept', 'Api\AcceptAnswerController');
+
+    Route::post('/questions/{question}/favorites', 'Api\FavoritesController@store');
+    Route::delete('/questions/{question}/favorites', 'Api\FavoritesController@destroy');
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
